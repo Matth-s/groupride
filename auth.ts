65126bda750980d60b6271f27a04022663633da8
Matthieu senosiain <senosiain.matthieu@yahoo.fr>
2024-08-16 10:09:23 +0200

    zuth trush auth en false

--- auth.ts
+++ auth.ts
@@ -43,7 +43,6 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return Boolean(auth);
     },
   },
-  trustHost: true,
   secret: process.env.AUTH_SECRET,
   ...authConfig,
 });
